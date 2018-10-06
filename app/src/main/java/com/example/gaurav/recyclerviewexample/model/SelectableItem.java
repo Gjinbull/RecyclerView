20deffe050c6d54b5e609fc5aa6f1615034430b0
gaurav <gauravgupta2111@gmail.com>
2018-10-07 00:35:57 +0530

    Commit again

---
+++ app/src/main/java/com/example/gaurav/recyclerviewexample/model/SelectableItem.java
@@ -0,0 +1,20 @@
+package com.example.gaurav.recyclerviewexample.model;
+
+public class SelectableItem extends Item {
+    private boolean isSelected = false;
+
+
+    public SelectableItem(Item item,boolean isSelected) {
+        super(item.getName(),item.getSurname());
+        this.isSelected = isSelected;
+    }
+
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
+    }
+}
