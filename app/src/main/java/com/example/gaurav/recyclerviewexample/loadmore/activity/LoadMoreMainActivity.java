c5d6e6a1ae25f0ad0d0fc36f6db369fd36f366ea
gaurav <gauravgupta2111@gmail.com>
2018-10-14 11:13:17 +0530

    Loadmore....

--- app/src/main/java/com/example/gaurav/recyclerviewexample/loadmore/activity/LoadMoreMainActivity.java
+++ app/src/main/java/com/example/gaurav/recyclerviewexample/loadmore/activity/LoadMoreMainActivity.java
@@ -3,10 +3,13 @@ package com.example.gaurav.recyclerviewexample.loadmore.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.example.gaurav.recyclerviewexample.R;
+
 public class LoadMoreMainActivity  extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_more_main);
     }
 }
