df3a270690abd8348ba8cbe8f4e21e4882457f64
gaurav <gauravgupta2111@gmail.com>
2018-10-14 11:07:18 +0530

    Delete packages and class from loadmore branch

--- app/src/main/java/com/example/gaurav/recyclerviewexample/loadmore/activity/LoadMoreMainActivity.java
+++ app/src/main/java/com/example/gaurav/recyclerviewexample/loadmore/activity/LoadMoreMainActivity.java
@@ -2,7 +2,6 @@ package com.example.gaurav.recyclerviewexample.loadmore.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import com.example.gaurav.recyclerviewexample.activity.BaseActivity;
 
 public class LoadMoreMainActivity  extends BaseActivity {
 
